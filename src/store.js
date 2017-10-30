5f6c405f9d16759590d8304df3eed0109dd5e943
Brian Demant <brian.demant@gmail.com>
2017-10-30 09:09:57 +0100

    added category support

--- src/store.js
+++ src/store.js
@@ -1,24 +1,109 @@
 const fetch = require('node-fetch')
+const { promisify } = require('util')
+const fs = require('fs')
+const [readFileAsync, writeFileAsync] = [promisify(fs.readFile), promisify(fs.writeFile)]
 
+let categoryData = {}
 
-let categoryData = {
-	"byId": {
-		"600": {
-			"id": 600,
-			"title": "Motorcykler og tilbehør",
-			"slug": "/transport/motorcykler-og-tilbehor/",
-			"parent_titles": [
-				"2-hjulet transport",
-			],
-		},
-	},
+const UPDATE_CATEGORY_INTEVAL = 1000 * 120
+
+async function loadAllCategories() {
+	let saved = {    }
+	try {
+		saved = JSON.parse(await readFileAsync("./categories.json", "utf8"))
+	} catch (e) {
+	}
+	console.log("saved.updated", saved.updated)
+
+	let secondsSinceLastUpdate = Date.now() - (saved.ts|0)
+	console.log("secondsSinceLastUpdate", secondsSinceLastUpdate)
+	if (secondsSinceLastUpdate > UPDATE_CATEGORY_INTEVAL) {
+		console.log("loadAllCategories")
+
+		const result = {
+			"ts": Date.now(),
+			"updated": new Date(),
+			"byId": {
+				"0": {
+					"id": 0,
+					"lvl": -1,
+					"title": "",
+					"slug": "/",
+					"parents": [],
+					"children": [],
+				},
+			},
+			"can_create": [],
+		}
+
+		categoryData = await loadCategory(result)
+		await writeFileAsync("./categories.json", JSON.stringify(categoryData, null, 2))
+		console.log("Done")
+	}
+	setTimeout(() => {
+		loadAllCategories()
+	}, UPDATE_CATEGORY_INTEVAL - secondsSinceLastUpdate + 100)
 }
 
- function loadCateoryTree() {
-
+async function loadSubCategories(id = 0) {
+	// console.log("loadSubCategories " + id)
+	const childResp = await fetch(`https://mit.guloggratis.dk/modules/gulgratis/ajax/ad_creator.fetch_categories_for_select.php?parent_categoryid=${id}`)
+	const children = (await childResp.json()).categories
+	return children.slice(0, 1)
 }
 
-console.log(await loadCateoryTree())
+async function loadCategory(result, lvl = 0, id = 0) {
+	// console.log("\nloadCategory " + id, lvl)
+	try {
+		if (id > 0) {
+			const catResp = await fetch(`https://api.guloggratis.dk/modules/gg_app/category/data?id=${id}`)
+			const cat = await catResp.json()
+			delete cat.adserving_keywords
+			delete cat.facets
+			delete cat.category_fields
+			// console.log("cat", cat)
+			result["byId"][id]["title"] = cat.name
+			result["byId"][id]["count"] = cat.results
+			result["byId"][id]["slug"] = cat.GAScreenValue
+
+			if (cat.payment_category) {
+				console.log("payment", id, cat.name)
+				console.log(cat.payment_category_info_text)
+			}
+			if (cat.can_create) {
+				result["can_create"].push(id)
+				result["byId"][id]["can_create"] = cat.can_create
+			}
+		}
+		// if (lvl > 2) return
+		if (!result["byId"][id]["can_create"]) {
+			const children = await loadSubCategories(id)
+			result["byId"][id]["children"] = children.map(x => x.categoryid)
+			for (let child of children) {
+				const childId = child.categoryid
+
+				result["byId"][childId] = {
+					id: childId,
+					lvl,
+					"title": child.name,
+					"count": 0,
+					"slug": "/",
+					"title_slug": child.path_as_string,
+					"parents": id > 0 ? [id, ...result["byId"][id].parents] : [],
+					"children": [],
+				}
+				await loadCategory(result, lvl + 1, childId)
+			}
+		}
+
+	} catch (e) {
+		console.log(e)
+	}
+
+	return result
+}
+
+
 class Category {
 	find(id) {
 		if (typeof id == "number")
@@ -69,6 +154,9 @@ class User {
 }
 
 module.exports = async () => {
+	loadAllCategories()
+
+
 	return await Promise.resolve({
 		Category: new Category(),
 		Listing: new Listing(),
