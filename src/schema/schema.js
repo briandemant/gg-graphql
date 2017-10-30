a8f8e54a58702e5b1845aaa1c43574535200640e
Brian Demant <brian.demant@gmail.com>
2017-10-30 09:44:24 +0100

    renamed schema file

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -2,8 +2,7 @@ const { makeExecutableSchema } = require('graphql-tools')
 const { readFileSync } = require('fs')
 
 const resolvers = require('./resolvers')
-const typeDefs = readFileSync(__dirname + "/schema.graphql", "utf-8")
+const typeDefs = readFileSync(__dirname + "/schema.graphqls", "utf-8")
 
 // Generate the schema object from your types definition.
-module.exports = (store) =>
-	makeExecutableSchema({ typeDefs, resolvers: resolvers(store) })
+module.exports = (store) => makeExecutableSchema({ typeDefs, resolvers: resolvers(store) })
